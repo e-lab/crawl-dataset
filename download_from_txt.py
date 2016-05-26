0e68f5622c0a4ee8c44c461bab7cbbe98b1f2f0f
spk <sangpilkim@purdue.edu>
2016-05-26 19:25:04 -0400

    fix bugs

--- download_from_txt.py
+++ download_from_txt.py
@@ -3,20 +3,26 @@ import os.path
 import sys
 
 def init():
-    if not os.path.isdir('./imges'):
+    if not os.path.isdir('./images'):
         init = sub.call('mkdir images',shell=True)
+    else:
+        print 'images folder exist'
 
 def makeFolder(name):
-    name = os.getcwd()+'/'+name
+    name = os.getcwd()+'/images/'+name
     if not os.path.isdir(name):
         print 'creating new images folder'
         make_folder = sub.call('mkdir -p '+name,shell=True)
+    else:
+        print name+' already exist'
+
 def moveFiles(name):
-    target = os.getcwd()+name
+    target = os.getcwd()+'/images/'+name
     if not os.path.isdir(target):
-        print 'movingfolder'
+        print 'moving images'
         makeFolder(name)
-        mv_files = sub.call('mv ./images/* '+target,shell=True)
+        mv_files = sub.call('mv images/*.png '+target,shell=True)
+
 def getImages(name):
     down = sub.call('node app.js '+name,shell=True)
 
@@ -27,11 +33,13 @@ def getNames(txt):
     for line in lines:
         con.append(line)
     return con
+
 if __name__=='__main__':
     names = getNames(sys.argv[1])
     init()
     for name in names:
         getImages(name)
+        makeFolder(name)
         moveFiles(name)
 
 
