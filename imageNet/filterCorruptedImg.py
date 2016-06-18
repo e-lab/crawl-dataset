18f9bd12f891255bc3fcc6c48a4096fd2b072ac5
spk <spkim921@gmail.com>
2016-06-18 06:58:45 -0400

    update filterCorrupted to do depth 2

--- imageNet/filterCorruptedImg.py
+++ imageNet/filterCorruptedImg.py
@@ -3,24 +3,35 @@
 
 from PIL import Image
 import os
+from os.path import join
 import fnmatch
 import subprocess as sub
 import sys
+from pathlib import Path as path
+di = []
 def filterCorruptPng(rootdir):
-    for subdir, dirs, files in os.walk(rootdir):
-        for fileName in files:
-            if '.jpg'in fileName:
-                filePath = os.path.join(rootdir,fileName)
-                try:
-                    v_image = Image.open(filePath)
-                    si = os.path.getsize(filePath)
-                    if si == 2051 or si < 15000:
-                        print fileName+' delete'
+    p = path(rootdir)
+    for x in p.iterdir():
+        if x.is_dir():
+            print('Working on this dir below')
+            print(str(x))
+            di.append(str(x))
+    for d in di:
+        for subdir, dirs, files in os.walk(d):
+            for fileName in files:
+                if '.png'in fileName:
+                    filePath = join(d,fileName)
+                    print (filePath+' check')
+                    try:
+                        v_image = Image.open(filePath)
+                        si = os.path.getsize(filePath)
+                        if si == 2051 or si < 15000:
+                            print (filePath+' delete')
+                            rm = sub.call('rm -rf '+filePath,shell=True)
+                    except:
+                        print (filePath+' delete')
                         rm = sub.call('rm -rf '+filePath,shell=True)
-                except:
-                    print fileName+' delete'
-                    rm = sub.call('rm -rf '+filePath,shell=True)
-
 if __name__ == '__main__':
     filterCorruptPng(sys.argv[1])
+
 
