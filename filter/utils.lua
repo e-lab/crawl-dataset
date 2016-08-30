5f0f06ce9f451d1a6257154a551fb9937f11ea31
spk <spkim921@gmail.com>
2016-08-30 16:42:04 -0400

    add missing utils

---
+++ filter/utils.lua
@@ -0,0 +1,15 @@
+function checkPaths(path, flag)
+--delete filtereddataset folder and creates a new one
+--paths.rmall(filteredDatasetPath, 'yes')
+--paths.mkdir(filteredDatasetPath)
+   if not paths.dirp(path) then
+      paths.mkdir(path)
+      --Flag for do classify
+      flag = true
+   else
+      print(path .. ' already exist')
+      flag = false
+   end
+   print('Flag : ' .. tostring(flag))
+   return flag
+end
