b2df9edb4d4824e0f9bfcd71a1fbd857f214410e
spk <spkim921@gmail.com>
2016-09-08 23:05:14 -0400

    add counter

--- tools/createStatic.py
+++ tools/createStatic.py
@@ -1,21 +1,33 @@
 import fnmatch
 import os
+import sys
 
 
-rootdir = '.'
+rootdir = sys.argv[1]
 folders = []
 for subdir, dirs, files in os.walk(rootdir):
     folders.append(subdir)
 folders = sorted(folders)
 
 img_nums = []
-ext = ['jpeg','png']
+ext = ['jpeg','png','JPEG']
+total = 0
+trainTotal = 0
+valTotal = 0
 for folder in folders:
     num = 0
     for ex in ext:
         num += len(fnmatch.filter(os.listdir(folder), '*.'+ex))
     img_nums.append(num)
+    if 'train' in folder:
+        trainTotal = trainTotal + num
+    if 'val' in folder:
+        valTotal = valTotal+ num
+    total = num + total
 
-with open("flickr_sta.tsv", "w") as record_file:
+with open(str(rootdir)+"dataset.tsv", "w") as record_file:
     for i in range(1,len(folders)):
         record_file.write(folders[i].replace('./','')+'\t'+str(img_nums[i]).replace(' ','')+'\n')
+    record_file.write('total :'+str(total)+'\n')
+    record_file.write('train :'+str(trainTotal)+'\n')
+    record_file.write('val   :'+str(valTotal)+'\n')
