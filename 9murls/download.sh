32684bd6452a51779ef28d604b010f9bd9b633cd
Eugenio Culurciello <culurciello@gmail.com>
2017-02-08 11:35:42 -0500

    Update download.sh

--- 9murls/download.sh
+++ 9murls/download.sh
@@ -8,7 +8,7 @@ wget https://storage.googleapis.com/openimages/2016_08/images_2016_08_v5.tar.gz
 wget https://storage.googleapis.com/openimages/2016_08/machine_ann_2016_08_v3.tar.gz
 wget https://storage.googleapis.com/openimages/2016_08/human_ann_2016_08_v3.tar.gz
 
-tar xf images_2016_08_v3.tar.gz && rm images_2016_08_v3.tar.gz
+tar xf images_2016_08_v5.tar.gz && rm images_2016_08_v5.tar.gz
 tar xf machine_ann_2016_08.tar.gz && rm machine_ann_2016_08.tar.gz
 tar xf human_ann_2016_08.tar.gz && rm human_ann_2016_08.tar.gz
 
